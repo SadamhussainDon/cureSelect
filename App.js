7249318ddccd05a662dfeeb9b64715ae7da6ef06
sadam <rayireactdeveloper@gmail.com>
2023-05-14 04:42:54 +0530

    fix the fix Redux and realm schema
    fix the fix Redux and realm schema

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Provider} from 'react-redux';
 import Navigation from './src/Navigation/Navigation';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import patientReducer from './src/reducer/patientReducer';
+const rootReducer = combineReducers({
+  patient: patientReducer,
+});
 
-export class App extends Component {
-  render() {
-    return (
-      // <Provider>
-      //   <Navigation />
-      // </Provider>
-      <Navigation />
-    );
-  }
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
+function App() {
+  return (
+    <Provider store={store}>
+        <Navigation />
+      </Provider>
+  );
 }
 
 export default App;
+
